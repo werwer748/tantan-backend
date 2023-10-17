4787c6dfc3673d1273792323d66e983b86ebca3c
hugo <werwer748@naver.com>
2023-10-18 00:42:04 +0900

    브런치복구중...
    origin/express-basic

--- hello-nest/src/cats/cats.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable() //? Injectable() 데코레이터를 사용하면 클래스를 NestJS가 관리하는 Provider로 변환해줌.
-export class CatsService {
-  hiCatServiceProduct() {
-    return 'hello cat!';
-  }
-}
